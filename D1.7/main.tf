e8d692e1c04a939bab45f38d0f464b46c1ceb473
vajierik <otr580@gmail.com>
2024-03-14 15:06:31 +0300

    add

---
+++ D1.7/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">=1.5.5"
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~>0.100.0"
+    }
+  }
+}
+
+provider "yandex" {
+  token     = "*вставить свое*"
+  cloud_id  = "* вставить свое*"
+  folder_id = "* встваить свое*"
+  zone      = "ru-central1-a"
+
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "swarm-network"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+module "swarm_cluster" {
+  source        = "./modules/instance"
+  vpc_subnet_id = yandex_vpc_subnet.subnet.id
+  managers      = 1
+  workers       = 2
+}
